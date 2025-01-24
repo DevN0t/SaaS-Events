e85b980f768c7902e548504af706cea09e4ce2da
cayna <cayna@agapesistemas.com.br>
2025-01-24 06:13:15 -0300

    fix: adjust cors

--- src/main/java/com/eventix/event/infra/security/config/WebConfig.java
+++ src/main/java/com/eventix/event/infra/security/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200/")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Origin", "Content-Type", "Accept", "responseType", "Authorization", "Access-Control-Allow-Origin", "cache-control");
     }
